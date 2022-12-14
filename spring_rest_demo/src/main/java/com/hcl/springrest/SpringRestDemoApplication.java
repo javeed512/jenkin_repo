308a761b8ad9274dfed2aef90927806242ca0c88
javeed512 <javeedmohammedjaveed@gmail.com>
2022-12-14 23:35:00 +0530

    main updated

--- spring_rest_demo/src/main/java/com/hcl/springrest/SpringRestDemoApplication.java
+++ spring_rest_demo/src/main/java/com/hcl/springrest/SpringRestDemoApplication.java
@@ -8,6 +8,9 @@ public class SpringRestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestDemoApplication.class, args);
+	
+
+		System.out.println("Updating main file");
 	}
 
 }
